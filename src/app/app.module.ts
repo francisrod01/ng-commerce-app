f9f301eedb2417b26367a2176d6542b058575440
Francis Rodrigues <francis.rod01@outlook.com>
2021-02-06 10:32:11 -0300

    wip: nav-bar component was generated by ng CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { SearchFormComponent } from './search-form/search-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
