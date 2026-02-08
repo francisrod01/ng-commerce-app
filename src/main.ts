ab4aecb997bcc932b4734a8e3f87784b9bbf77b2
Francis Batista <francisrod01@users.noreply.github.com>
2026-02-08 02:15:33 -0300

    fix(main): cast environment providers for bootstrapModule to avoid TS mismatch

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection, StaticProvider } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+platformBrowserDynamic().bootstrapModule(AppModule, { providers: [provideZoneChangeDetection() as unknown as StaticProvider] })
   .catch(err => console.error(err));
